69d266bad4ee906934206be009a6ddaa09ed5815
reddy <reddymeghraj@gmail.com>
2015-06-02 12:31:13 +0530

    [SocietyMaster For Society Management]

--- societymaster/society_master/doctype/add_owner/add_owner.py
+++ societymaster/society_master/doctype/add_owner/add_owner.py
@@ -6,7 +6,20 @@ import frappe
 from frappe.model.document import Document
 
 class AddOwner(Document):
-	pass
+	def validate(self):
+		name=self.owner_name
+		email=self.email_id
+		c=frappe.db.sql("""select email_id from `tabContact` where first_name=%s and email_id=%s""",(name,email))
+		if c:
+			frappe.throw("Entered Name and Email id already exists!")
+		else:
+			n=frappe.db.sql("""select max(cast(name as int)) from `tabContact`""")[0][0]
+			if n:
+				num=int(n)+1
+			else:
+				num=1
+			s=frappe.db.sql("""insert into `tabContact` set name=%s,first_name=%s, email_id=%s""",(num,name,email))
+
 @frappe.whitelist()
 def check_house(l,w,h):
 	q=frappe.db.sql("""select select_lane,select_wing,select_htype from `tabAdd Owner` where select_lane=%s and select_wing=%s and select_htype=%s""",(l,w,h))
